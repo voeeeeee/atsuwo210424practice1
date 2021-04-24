5ff6ef1350ee5051222897923d1f315ee7d088de
Atsushi.Taketsuji <app@commude.ph>
2021-04-24 20:37:51 +0900

    課題1作成

--- practice1/ViewController.swift
+++ practice1/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var text1: UITextField!
+    @IBOutlet weak var text2: UITextField!
+    @IBOutlet weak var text3: UITextField!
+    @IBOutlet weak var text4: UITextField!
+    @IBOutlet weak var text5: UITextField!
+    
+    
+    @IBOutlet weak var label: UILabel!
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func sumNumber(_ sender: Any) {
+        let num1:Int = Int(String(text1.text ?? "")) ?? 0
+        let num2:Int = Int(String(text2.text ?? "")) ?? 0
+        let num3:Int = Int(String(text3.text ?? "")) ?? 0
+        let num4:Int = Int(String(text4.text ?? "")) ?? 0
+        let num5:Int = Int(String(text5.text ?? "")) ?? 0
+        
+        
+        label.text = String(num1 + num2 + num3 + num4 + num5)
+    }
+    
+    
 
 }
 
